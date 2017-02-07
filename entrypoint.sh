c102e368037218c21bd8cc8f27af0a5c1e8e33f4
Benjamin A. Laken <benlaken@gmail.com>
2017-02-07 14:34:07 +0100

    Minimally running setup. Should run in deploy mode on server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+echo "Running python process"
 echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.pem
-python main.py
+#exec gunicorn -w 2 main:app
+exec python main.py
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from __future__ import print_function, division
-from flask import Flask, jsonify, request
-from flask_restful import reqparse, abort, Api, Resource
+import flask
+import flask_restful
 import sys
 import os
 import ee
 
-# If using a local mac, assume you can initilise using the below...
-print("Starting Flask Microservice")
-print("RUNNING ON ", sys.platform)
+app = flask.Flask(__name__)
+api = flask_restful.Api(app)
+
+print("Starting Flask Microservice. Running on ", sys.platform)
 if sys.platform == 'darwin':
+    # If using a local mac, assume you can initilise using the below...
     ee.Initialize()
 else:
     # Else, assume you have an EE_private_key environment variable with authorisation,
@@ -17,58 +19,65 @@ else:
     credentials = ee.ServiceAccountCredentials(service_account, './privatekey.pem')
     ee.Initialize(credentials, 'https://earthengine.googleapis.com')
 
-app = Flask(__name__)
-api = Api(app)
+class ClickPointData(flask_restful.Resource):
+    def __init__(self):
+        self.params = ['lat', 'lon', 'z']
+        # Hacky way of ensuring the area of polygon does not become too small to function as a reducer
+        self.z_dic = [156412, 78206, 3910, 19551, 9776, 4888, 7000, 7000, 7000, 7000, 7000, 7000, 7000]
+        self.imageIDs = ['users/malariaatlasproject/accessibilityMap/jrc_accesibility2008',
+                       'users/malariaatlasproject/accessibilityMap/jrc_accessibility2017']
+        self.band = 'b1'
+        return
 
-params = ['lat', 'lon', 'z']
-z_dic = [156412, 78206, 3910, 19551, 9776, 4888, 2444, 1222, 610.984, 305.492, 152.746, 76.373, 38.187]
-data_2008 = 'users/malariaatlasproject/accessibilityMap/jrc_accesibility2008'
-data_2017 = 'users/malariaatlasproject/accessibilityMap/jrc_accessibility2017'
-test_data = "USGS/SRTMGL1_003"
-band = 'b1'
-
-
-# route for returning click point data
-def check_request_params(request):
-    if request.has_key(params[0]) and request.has_key(params[1]) and request.has_key(params[2]):
-        print('Valid parmas')
-    else:
-        abort(404, message="Request {} must contain lat, lng, and z.".format(request))
-
-
-# class to handle click request
-class ClickPointData(Resource):
     def post(self):
-        location = request.get_json(force=True)
-        check_request_params(location)
-        ee_stats = return_ee_stats(location)
+        location = flask.request.get_json(force=True)
+        self.check_request_params(location)
+        ee_stats = self.return_ee_stats(location)
         print("Got {0}".format(ee_stats))
         return ee_stats
 
+    def check_request_params(self, request):
+        """route for returning click point data"""
+        if request.has_key(self.params[0]) and request.has_key(self.params[1]) and request.has_key(self.params[2]):
+            print('Valid parameters passed')
+        else:
+            flask_restful.abort(404, message="Request {} must contain lat, lon, and z.".format(request))
+        return
 
-def return_ee_stats(location):
-    """Request focused data from EE from params"""
-    distance = z_dic[location['z']]
-    d_point = {}
-    d_point['lon'] = location['lon']
-    d_point['lat'] = location['lat']
-    d_point['opt_proj'] = 'EPSG:4326'
-    d_point['opt_geodesic'] = False
-    point_of_interest = ee.Geometry.Point(**d_point).buffer(distance)
-    # print("Area of buffered point = {} m^2".format(point_of_interest.area().getInfo()))
-    d = {}
-    d['bestEffort'] = True
-    d['geometry'] = point_of_interest
-    d['reducer'] = ee.Reducer.count() \
-        .combine(ee.Reducer.sum(), outputPrefix='', sharedInputs=True) \
-        .combine(ee.Reducer.mean(), outputPrefix='', sharedInputs=True) \
-        .combine(ee.Reducer.sampleStdDev(), outputPrefix='', sharedInputs=True) \
-        .combine(ee.Reducer.min(), outputPrefix='', sharedInputs=True) \
-        .combine(ee.Reducer.max(), outputPrefix='', sharedInputs=True)
-    return ee.Image(data_2008).select(band).reduceRegion(**d).getInfo()
+    def eePoint(self, location):
+        """Return an Earth Engine Point, Buffered by a distance set by the z-level"""
+        distance = self.z_dic[location['z']]
+        d_point = {'lon': location['lon'], 'lat': location['lat'], 'opt_proj': 'EPSG:4326', 'opt_geodesic': False}
+        return ee.Geometry.Point(**d_point).buffer(distance)
 
-# set up routes/resources
+    def return_ee_stats(self, location):
+        """
+        Request aggregated data from two EE images, and place into a single dictionary (response_dict).
+        We also control the sig figs of the returned data via a string and format command here.
+        """
+        response_dict = {}
+        d = {'bestEffort': True, 'geometry': self.eePoint(location), 'reducer': ee.Reducer.mean()}
+        for image in self.imageIDs:
+            image_key = image.split('/')[-1][-4:]
+            print("Requesting EE data for {0}".format(image_key))
+            # print("Set buffer distance of ", self.z_dic[location['z']])
+            try:
+                response = ee.Image(image).select(self.band).reduceRegion(**d).getInfo()
+                if response['b1']:
+                    response_dict[image_key + '_mean'] = '{0:6.2f}'.format(response['b1'])
+                else:
+                    response_dict[image_key + '_mean'] = 'null'
+            except ee.EEException:
+                print("Hit EEException with request")
+                response_dict[image_key + '_mean'] = 'null'
+        return response_dict
+
+
+# set up routes
 api.add_resource(ClickPointData, '/api/click-point-data/')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=os.getenv('DEBUG') == 'True')
+    if sys.platform == 'darwin':
+        app.run(host='0.0.0.0', debug=os.getenv('DEBUG') == 'True')
+    else:
+        app.run(host='0.0.0.0', debug=os.getenv('DEBUG') == 'False')
