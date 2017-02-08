b76c49ae26c320aab4a39c51da6ee8af2771da6e
edbrett <e.j.a.brett@gmail.com>
2017-02-08 11:52:33 +0100

    set start for dev

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ NAME=flask_app
 docker build -t $NAME --build-arg NAME=$NAME .
 #docker run -it -v $(pwd)/data:/opt/$NAME/data --env-file .env --rm $NAME "/bin/bash"
 chmod +x entrypoint.sh
-docker run -v $(pwd):/opt/$NAME -p 8000:5000 --restart always --env-file .env --rm $NAME
+docker run -v $(pwd):/opt/$NAME -p 8000:5000 --env-file .env --rm $NAME
