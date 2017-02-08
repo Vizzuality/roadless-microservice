2bb285f4d9626143a78102532206898e17d7729f
Benjamin A. Laken <benlaken@gmail.com>
2017-02-08 14:30:06 +0100

    With Sergio, I set up docker-compose methods of running a production and development server for the mircroservice. The production server uses Gunicorn to make better Python processes. Run these with `$./start <production>` or `$./start <development>`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+set -e
 
-echo "Running python process"
-echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.pem
-#exec gunicorn -w 2 main:app
-exec python main.py
+case "$1" in
+    develop)
+        echo "Running Development Server"
+        echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.pem
+        exec python main.py
+        ;;
+    test)
+        echo "Test (not yet)"
+        ;;
+    production)
+        echo "Running Production Server"
+        echo -e "$EE_PRIVATE_KEY" | base64 -d > privatekey.pem
+        #exec gunicorn -w 2 main:app
+        exec gunicorn -c gunicorn.py main:app
+        ;;
+    *)
+        exec "$@"
+esac
